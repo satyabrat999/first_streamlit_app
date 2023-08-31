7d53d2725f0afd5a72f1d67408ffb5d330832fa7
satyabrat999 <143700416+satyabrat999@users.noreply.github.com>
2023-08-31 14:27:56 +0530

    Update streamlit_app.py
    Added more options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 and blueberry Oatmeal')
+streamlit.text('Kale Spinache and Rocket smoothie')
+streamlit.text('Hrd Boiled Free-Range Egg')
+
